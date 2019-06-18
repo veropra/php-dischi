45cb93c9b6a2c34cf86c93affb2b6419229ea264
veropra <prainoveronica@gmail.com>
2019-06-18 19:43:38 +0200

    Fase 1 ok, bug presente...error 500!

---
+++ data.php
@@ -0,0 +1,59 @@
+<?php
+  $albums = [
+    [
+      'img'=> 'https://i.ytimg.com/vi/EZyGWg0uqkw/0.jpg',
+      'album'=> 'Only by the night',
+      'musical_group' => 'King of Leon',
+      'year'=> '2008/2009',
+    ],
+    [ 'img'=> 'http://cdn.shopify.com/s/files/1/1353/8123/products/5099751076513_1024x1024.jpg?v=1512022424',
+      'album'=> 'Back in black',
+      'musical_group'=> 'AC/DC',
+      'year'=> '1980',
+    ],
+    [
+      'img'=> 'http://static.lafeltrinelli.it/static/images-3/xxl/395/1712395.jpg',
+      'album'=> 'Toxicity',
+      'musical_group'=> 'System of a Down',
+      'year'=> '2001',
+    ],
+    [ 'img'=> 'https://static.lafeltrinelli.it/static/images-3/xxl/487/1456487.jpg',
+      'album'=> 'A kind of magic',
+      'musical_group'=> 'Queen',
+      'year'=> '1986',
+    ],
+    [
+      'img'=> 'https://images-na.ssl-images-amazon.com/images/I/910RnSsMT6L._SX466_.jpg',
+      'album'=> 'Devils got a a new disguise',
+      'musical_group'=> 'Aerosmith',
+      'year'=> '2006',
+    ],
+    [
+      'img'=> 'https://www.discoservicemusicheria.com/shop/wp-content/uploads/2013/02/Linkin-Park-Meteora.jpg',
+      'album'=> 'Meteora'
+      'musical_group'=> 'Linkin Park',
+      'year'=> '2003',
+    ],
+    [ 'img'=> 'https://images-na.ssl-images-amazon.com/images/I/81bcusKozsL._SX466_.jpg',
+      'album'=> 'Wasting light'
+      'musical_group'=> 'Foo Fighters',
+      'year'=> '2011',
+    ],
+    [ 'img'=> 'https://m.media-amazon.com/images/I/81JoImtwFhL._SS500_.jpg',
+      'album'=> 'Triller',
+      'musical_group'=> 'Michael Jackson',
+      'year'=> '1982',
+    ],
+    [ 'img'=> 'https://images-na.ssl-images-amazon.com/images/I/81BVsdXqg6L._SY355_.jpg',
+      'album'=> 'Brave new World',
+      'musical_group'=> 'Iron Maiden',
+      'year'=> '2000',
+    ],
+    [
+      'img'=> 'https://www.bonjoviclubitalia.com/wp-content/uploads/2016/05/crush-ablumart.jpg',
+      'album'=>'Crush',
+      'musical_group'=> 'Bon Jovi',
+      'year'=> '2000',
+    ],
+  ]
+ ?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include 'data.php' ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>php-dischi</title>
+    <link rel="stylesheet" href="public/css/php-dischi.css">
+  </head>
+  <body>
+    <?php
+      foreach ($albums as $key) {
+    ?>
+  </body>
+</html>
